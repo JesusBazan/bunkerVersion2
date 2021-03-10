fb053c4794818a8a413a516ac7afd19766d01179
EmmanuelBazan <mqkz99@gmail.com>
2021-03-09 22:34:23 -0600

    importacion de inputs de material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//imports de Material Desidn
 import {MatIconModule} from '@angular/material/icon';
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import{FormsModule,ReactiveFormsModule}from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MatematicasComponent } from './components/matematicas/matematicas.component';
 import { SideNavMaterialComponent } from './side-nav-material/side-nav-material.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+
 
 import { MateriasComponent } from './components/materias/materias.component';
 import { LibrosComponent } from './components/libros/libros.component';
 import { NewsComponent } from './components/news/news.component';
-import { MatCardModule } from '@angular/material/card';
 import { CardNewsComponent } from './components/card-news/card-news.component';
 import { AnunciosService} from './services/anuncios/anuncios.service'
 import { InicioComponent } from './components/inicio/inicio.component';
@@ -29,7 +36,6 @@ import { LoginFormComponent} from './components/login-form/login-form.component'
 
 import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { FirmarComponent } from './components/firmar/firmar.component';
-import{FormsModule,ReactiveFormsModule}from '@angular/forms';
 import { from } from 'rxjs';
 @NgModule({
   declarations: [
@@ -61,8 +67,9 @@ import { from } from 'rxjs';
     MatCardModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserModule
-  
+    BrowserModule,
+    MatInputModule,
+    MatFormFieldModule
     
   ],
   providers: [
