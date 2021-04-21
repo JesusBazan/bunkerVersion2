ff4bb1799d3cfecde9068424d758cb885ef7dc7e
EmmanuelBazan <mqkz99@gmail.com>
2021-04-20 23:45:56 -0500

    reporte de mensaje firmado

--- src/app/components/firmar/firmar.component.ts
+++ src/app/components/firmar/firmar.component.ts
@@ -2,6 +2,9 @@ import { sha1 } from '@angular/compiler/src/i18n/digest';
 import { Component, OnInit } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
 
+import {UsuariosService} from '../../services/usuarios/usuarios.service'
+import {ReportesService} from '../../services/reportes/reportes.service'
+
 @Component({
   selector: 'app-firmar',
   templateUrl: './firmar.component.html',
@@ -17,9 +20,21 @@ export class FirmarComponent implements OnInit {
   textoDesencriptado: any;
   ver:string;
   verifica:any;
-  constructor() { }
+  constructor(private usuarioService:UsuariosService, private reporteService:ReportesService) { }
 
   convertirTexto(conversion: string) {
+
+    //operaciones para crear reporte de la accion ejecutada
+    this.reporteService.reporte.accion = conversion;
+    this.reporteService.reporte.fk_usuario = this.usuarioService.usuarioActual.id;
+    this.reporteService.insertarReporte().subscribe(
+      res => {
+        console.log(res)
+      },
+      err => {
+        console.log(err)
+      }
+    )
 
     this.encPass = 0;
     if (conversion === 'encriptar') {
@@ -50,6 +65,8 @@ export class FirmarComponent implements OnInit {
   ngOnInit(): void {
 
     this.convertirTexto;
+
+    this.reporteService.reporte.herramienta = 'FIRMADO';
   }
 
 }
